fe95104c26af2cacee1f6446671b7fef3d21049c
l3miage-bouchelm <mahrezmus@gmail.com>
2024-04-18 13:20:28 +0200

    ajout authentification firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { Auth, User, authState, signOut, signInWithPopup, GoogleAuthProvider } from '@angular/fire/auth';
+import { Signal, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +19,34 @@ import { MatListModule } from '@angular/material/list';
   imports: [
     RouterOutlet, RouterModule,
     MatToolbarModule, MatListModule,
-    MatSidenavModule, MatIconModule, MatButtonModule,
+    MatSidenavModule, MatIconModule, MatButtonModule, CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private fbAuth: Auth) { } 
+  readonly obsUser: Observable<User | null> = authState(this.fbAuth) 
+  //readonly userSignal : new Signal<boolean>();
+  //readonly userSignal = this.obsUser.pipe(map(user => !!user))
+  async logout(): Promise<void> { 
+
+       return signOut(this.fbAuth); 
+    } 
+    async loginGoogle(): Promise<void> { 
+    
+        return signInWithPopup(this.fbAuth, new GoogleAuthProvider()).then( 
+    
+        console.log, 
+    
+        console.error 
+    
+      ) 
+    
+      } 
 }
+
+
+
+
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimationsAsync(),
+    provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"projetintegrateur9","appId":"1:92324758253:web:5c21ba427087c105cc42e4","storageBucket":"projetintegrateur9.appspot.com","apiKey":"AIzaSyD-cMegyM3cA_cUPGE8RBP9L0mfhOOyEgs","authDomain":"projetintegrateur9.firebaseapp.com","messagingSenderId":"92324758253","measurementId":"G-YW0Q9NHHWT"}))), importProvidersFrom(provideAuth(() => getAuth())),
   ],
 };
