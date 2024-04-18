f5d81d9bb22363d354b39ee13e578c44748c0bc1
l3miage-liuxi <xiaoman.liu@etu.univ-grenoble-alpes.fr>
2024-04-18 11:27:48 +0200

    PartitionAlea_version2

--- server/src/main/java/l3m/cyber/planner/utils/PartitionAlea.java
+++ server/src/main/java/l3m/cyber/planner/utils/PartitionAlea.java
@@ -1,18 +1,19 @@
 package l3m.cyber.planner.utils;
-import java.util.ArrayList;
 import java.util.Random;
 
 public class PartitionAlea extends Partition {
-    public PartitionAlea(ArrayList<Integer> elems, int k, int elemSpecial) {
-        super(elems, k, elemSpecial);
+    public PartitionAlea(int n,int k) {
+        super(n, k);
     }
 
 
     @Override
     public void partitionne(double[][] distances) {
-        elems.remove(elemSpecial);
         Random random = new Random();
-        for(int i = 0; i<k;i++){
+        for(int i = 0; i<this.nbElem;i++){
+            if (elems.get(i) == 0) {
+                continue; // si l'element est 0, on saute dans la prochain iteration
+            }
             int randomPartie = random.nextInt(this.k);
             parties.get(randomPartie).add(elems.get(i));
         }
