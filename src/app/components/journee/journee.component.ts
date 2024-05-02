8aa53e2944e75c3bd1f908c4d71237295b31f030
Ding Zichen <Zichen.Ding@etu.univ-grenoble-alpes.fr>
2024-05-02 16:07:39 +0200

    constuireJ

--- src/app/components/journee/journee.component.ts
+++ src/app/components/journee/journee.component.ts
@@ -4,6 +4,8 @@ import { HttpClient,HttpClientModule } from '@angular/common/http';
 import { AfterViewInit, ChangeDetectionStrategy, Component, QueryList, ViewChildren, WritableSignal, computed, signal } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { CommandeService } from '../../services/commande.service';
+import { TourneeComponent } from '../tournee/tournee.component';
+import { FormsModule } from '@angular/forms';
 
 interface ListDD_Data<T> {
   readonly sig: WritableSignal<readonly T[]>;
@@ -15,17 +17,23 @@ interface ListDD_Data<T> {
   standalone: true,
   imports: [
     CommonModule, MatListModule,
-    CdkDropList, CdkDrag,HttpClientModule
+    CdkDropList, CdkDrag,HttpClientModule,TourneeComponent,FormsModule
   ],
   templateUrl: './journee.component.html',
   styleUrl: './journee.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class JourneeComponent implements AfterViewInit {
-  commande:Commande[]=[];
-  livreur:Livreur[]=[];
+  commandes:Commande[]=[];
+  livreurs:Livreur[]=[];
+  camions:Camion[]=[];
+  entrepots:Entrepot[]=[];
   commandeOuvert:Commande[]=[];
-  commandePrevu:Commande[]=[];
+  commandePrevu:Commandes[]=[];
+  tournees:Tournee[]=[];
+  entrepotChoisi:string='';
+
+
   constructor(private http:HttpClient) { }
 
   readonly sigL1 = signal<readonly string[]>(["A", "B", "C", "D"])
@@ -40,6 +48,7 @@ export class JourneeComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     this.getCsvData();
+    this.lireInfos();
     const update = () => {
       const L = this.matLists.toArray();
       this._sigDropListRefs.set(L)
@@ -71,16 +80,16 @@ export class JourneeComponent implements AfterViewInit {
   getCsvData(){
     this.http.get('./assets/Export_Commandes.csv',{responseType:'text'})
     .subscribe(data=>{
-      this.commande=this.parseCsvData(data);
+      this.commandes=this.lireCommande(data);
     },
   );
-    this.commandeOuvert=this.commande.filter(commande => commande.etat === "ouverte");
+    this.commandeOuvert=this.commandes.filter(commande => commande.etat === "ouverte");
     this.sigCO.set(this.commandeOuvert.map(commande => commande.reference));
     this.sigCP.set([]);
     this.commandePrevu=[];
   }
 
-  parseCsvData(csvData:string){
+  lireCommande(csvData:string){
     const lines=csvData.split('\n');
     const result:Commande[]= [];
 
@@ -105,27 +114,146 @@ export class JourneeComponent implements AfterViewInit {
     return result;
   }
 
+  lireLivreur(csvData:string){
+    const lines=csvData.split('\n');
+    const result:Livreur[]= [];
+
+    const headers=lines[0].split(',');
+    for(let i=1;i<lines.length;i++){
+      const obj: ParsedData = {};
+      const currentLine=lines[i].split(',');
+      for(let j=0;j<headers.length;j++){
+        obj[headers[j]]=currentLine[j];
+      }
+      const livreur:Livreur={
+        trigramme:obj[headers[0]],
+        prenom:obj[headers[1]],
+        nom:obj[headers[2]],
+        photo:obj[headers[3]],
+        telephone:obj[headers[4]],
+        emploi:obj[headers[5]],
+        entrepot:obj[headers[6]],
+        tournees:obj[headers[7]],
+      }
+      if(livreur.emploi=='livreur'){
+        result.push(livreur);
+      }     
+    }
+    return result;
+  }
+
+  lireCamion(csvData:string){
+    const lines=csvData.split('\n');
+    const result:Camion[]= [];
+
+    const headers=lines[0].split(',');
+    for(let i=1;i<lines.length;i++){
+      const obj: ParsedData = {};
+      const currentLine=lines[i].split(',');
+      for(let j=0;j<headers.length;j++){
+        obj[headers[j]]=currentLine[j];
+      }
+      const camion:Camion={
+        immatriculation:obj[headers[0]],
+        latitude:obj[headers[1]],
+        longitude:obj[headers[2]],
+        entrepot:obj[headers[3]],
+      }
+      result.push(camion);    
+    }
+    return result;
+  }
+
+  lireEntrepot(csvData:string){
+    const lines=csvData.split('\n');
+    const result:Entrepot[]= [];
+
+    const headers=lines[0].split(',');
+    for(let i=1;i<lines.length;i++){
+      const obj: ParsedData = {};
+      const currentLine=lines[i].split(',');
+      for(let j=0;j<headers.length;j++){
+        obj[headers[j]]=currentLine[j];
+      }
+      const entrepot:Entrepot={
+        nom:obj[headers[0]],
+        lettre:obj[headers[1]],
+        photo:obj[headers[2]],
+        adresse:obj[headers[3]],
+        codePostal:obj[headers[4]],
+        ville:obj[headers[5]],
+        latitude:obj[headers[6]],
+        longitude:obj[headers[7]],
+        camions:obj[headers[8]],
+        employés:obj[headers[9]],
+      }
+      result.push(entrepot);   
+    }
+    return result;
+  }
+
+  lireInfos(){
+    this.http.get('./assets/Export_Employés.csv',{responseType:'text'})
+    .subscribe(data=>{
+      this.livreurs=this.lireLivreur(data);
+    },
+  );
+  this.http.get('./assets/Export_Camions.csv',{responseType:'text'})
+    .subscribe(data=>{
+      this.camions=this.lireCamion(data);
+    },
+  );
+  this.http.get('./assets/Export_Entrepôts.csv',{responseType:'text'})
+    .subscribe(data=>{
+      this.entrepots=this.lireEntrepot(data);
+    },
+  );
+  }
+
   changeEtat(){
     for(let i=0;i<this.sigCP().length;i++){
       for(let j=0;j<this.commandeOuvert.length;j++){
         if(this.sigCP()[i]==this.commandeOuvert[j].reference){
           this.commandeOuvert[j].etat='prévu';
-          this.commandePrevu.push(this.commandeOuvert[j]);
+          this.commandePrevu.push({commande:this.commandeOuvert[j],disabled:false,tourneeID:0});
           this.commandeOuvert.splice(j,1);
         }
       }
     }
     for(let i=0;i<this.sigCO().length;i++){
       for(let j=0;j<this.commandePrevu.length;j++){
-        if(this.sigCO()[i]==this.commandePrevu[j].reference){
-          this.commandePrevu[j].etat='ouvert';
-          this.commandeOuvert.push(this.commandePrevu[j]);
+        if(this.sigCO()[i]==this.commandePrevu[j].commande.reference){
+          this.commandePrevu[j].commande.etat='ouvert';
+          this.commandeOuvert.push(this.commandePrevu[j].commande);
           this.commandePrevu.splice(j,1);
         }
       }
     }
   }
 
+
+  addTournee() {
+    this.tournees.push({id:this.tournees.length+1,commandes:[]});
+  }
+
+  removeTournee(index: number) {
+    this.tournees.splice(index, 1);
+  }
+
+  toggleSelectionCommande(item: Commandes,itemSelected :Commande[],i:number) {
+    if (itemSelected.indexOf(item.commande)===-1) {
+      itemSelected.push(item.commande);
+      item.disabled=!item.disabled;
+      item.tourneeID=i;
+    } else {
+      const index = itemSelected.indexOf(item.commande);
+      if (index !== -1) {
+        itemSelected.splice(index, 1);
+        item.disabled=!item.disabled;
+        item.tourneeID=0;
+      }
+    }
+  }
 
 }
 
@@ -155,4 +283,35 @@ interface Livreur {
   emploi: string;
   entrepot: string;
   tournees: string;
+}
+
+interface Tournee {
+  id:number;
+  commandes:Commande[];
+}
+
+interface Camion{
+  immatriculation:string;
+  latitude:string;
+  longitude:string;
+  entrepot:string;
+}
+
+interface Entrepot{
+  nom:string;
+  lettre:string;
+  photo:string;
+  adresse:string;
+  codePostal:string;
+  ville:string;
+  latitude:string;
+  longitude:string;
+  camions:string;
+  employés:string;
+}
+
+interface Commandes{
+  commande:Commande;
+  disabled:boolean;
+  tourneeID:number;
 }
--- src/app/components/tournee/tournee.component.ts
+++ src/app/components/tournee/tournee.component.ts
@@ -1,12 +1,100 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tournee',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './tournee.component.html',
   styleUrl: './tournee.component.scss'
 })
 export class TourneeComponent {
+  @Input()
+  livreurs:Livreur[]=[]
+  @Input()
+  camions:Camion[]=[]
+  @Input()
+  entrepots:Entrepot[]=[]
+  @Input()
+  commandes:Commande[]=[]
 
+  constructor() { }
+  entrepotChoisi:string='default';
+  camionsChoisi:Camion[]=[];
+  livreursChoisi:Livreur[]=[];
+
+
+
+  toggleSelection(item: any,itemSelected :any[]) {
+    if (itemSelected.indexOf(item)===-1) {
+      itemSelected.push(item);
+    } else {
+      const index = itemSelected.indexOf(item);
+      if (index !== -1) {
+        itemSelected.splice(index, 1);
+      }
+    }
+  }
+
+  toggleSelectionCommande(item: Commandes,itemSelected :Commande[]) {
+    if (itemSelected.indexOf(item.commande)===-1) {
+      itemSelected.push(item.commande);
+      item.disabled=!item.disabled;
+    } else {
+      const index = itemSelected.indexOf(item.commande);
+      if (index !== -1) {
+        itemSelected.splice(index, 1);
+        item.disabled=!item.disabled;
+      }
+    }
+  }
+
+  
+}
+
+interface Livreur {
+  trigramme: string;
+  prenom: string;
+  nom: string;
+  photo: string;
+  telephone: string;
+  emploi: string;
+  entrepot: string;
+  tournees: string;
+}
+
+interface Camion{
+  immatriculation:string;
+  latitude:string;
+  longitude:string;
+  entrepot:string;
+}
+
+interface Entrepot{
+  nom:string;
+  lettre:string;
+  photo:string;
+  adresse:string;
+  codePostal:string;
+  ville:string;
+  latitude:string;
+  longitude:string;
+  camions:string;
+  employés:string;
+}
+
+interface Commande {
+  reference: string;
+  etat: string;
+  dateDeCreation: string;
+  note: string;
+  commantaire: string;
+  client: string;
+  ligne: string;
+}
+
+interface Commandes{
+  commande:Commande;
+  disabled:boolean;
 }
